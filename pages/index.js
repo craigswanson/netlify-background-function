db5f1a3f9925986bc04d30f91a239c8b958deaf4
Craig Swanson <craig@craigswanson.org>
2023-05-08 15:28:24 -0700

    Add debugging logs to index.js

--- pages/index.js
+++ pages/index.js
@@ -6,30 +6,36 @@ export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    try {
-      const response = await fetch("/.netlify/functions/generator-background", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ animal: animalInput }),
-      });
+async function onSubmit(event) {
+  event.preventDefault();
+  try {
+    const response = await fetch("/.netlify/functions/generator-background", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ animal: animalInput }),
+    });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+    if (response.status !== 200) {
+      throw new Error(`Request failed with status ${response.status}`);
     }
+
+    // Log the response text for debugging purposes
+    const responseText = await response.text();
+    console.log("Response text:", responseText);
+
+    // Parse the response text as JSON
+    const data = JSON.parse(responseText);
+
+    setResult(data.result);
+    setAnimalInput("");
+  } catch (error) {
+    // Consider implementing your own error handling logic here
+    console.error(error);
+    alert(error.message);
   }
+}
 
   return (
     <div>
